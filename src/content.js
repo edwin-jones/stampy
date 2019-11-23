b4d74f2fc293f4f1aa407bacfde5dfff94a7f678
Edwin Jones <edwinj85@gmail.com>
2019-11-23 19:23:57 +0000

    optimise content parser

--- src/content.js
+++ src/content.js
@@ -1,26 +1,37 @@
-console.info("stampy - running unix timestamp conversion for page");
+// use an anon function to avoid polluting the environment after the script is run
+(function() {
+    console.info("stampy - running unix timestamp conversion for page");
 
-const timerName = "stampy - document body parsing took";
-console.time(timerName);
+    const timerName = "stampy - document body parsing took";
+    console.time(timerName);
 
-const replaceTimestamp = function (value) {
-    let date = new Date(value * 1000);
-    let locale = window.navigator.userLanguage || window.navigator.language;
-    let replacement = `${date.toLocaleDateString(locale)} @ ${date.toLocaleTimeString(locale)} (UTC)`;
+    function replaceTimestamp(value) {
+        let date = new Date(value * 1000);
+        let locale = window.navigator.language;
+        let replacement = `${date.toLocaleDateString(locale)} @ ${date.toLocaleTimeString(locale)} (UTC)`;
 
-    return replacement;
-}
+        return replacement;
+    }
+
+    function filterInvalidTextNodes(node) {
+
+        // invisible elements should not be considered for timestamp conversion
+        if (node.parentElement.style.display === 'none') {
+            return NodeFilter.FILTER_SKIP;
+        }
+
+        return NodeFilter.FILTER_ACCEPT;
+    }
 
 
-const timestampRegex = RegExp("[0-9]{10}");
-const nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
+    const timestampRegex = RegExp("[0-9]{10}");
+    const nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT, filterInvalidTextNodes);
 
-let matchCounter = 0;
-let node;
+    let node;
+    while (node = nodeIterator.nextNode()) {
+        node.textContent = node.textContent.replace(timestampRegex, replaceTimestamp);
+    }
 
-while (node = nodeIterator.nextNode()) {
-    node.textContent = node.textContent.replace(timestampRegex, replaceTimestamp);
-}
-
-console.timeEnd(timerName);
-console.info(`stampy - timestamp conversion completed with ${matchCounter} text node(s) altered`);
+    console.timeEnd(timerName);
+    console.info(`stampy - timestamp conversion completed`);
+})();
